695b503e893a452b501d2cec92d8bf3cf6d2fb87
unknown <lucasuchoalg@gmail.com>
2020-10-08 16:24:16 -0300

    any

--- app_uchoa/urls.py
+++ app_uchoa/urls.py
@@ -1,43 +1,51 @@
-from django.contrib import admin
-from django.urls import path, include
-
-from rest_framework import routers
-
-from .views import (home,
-    cliente, 
-    clientedetail, 
-    emprestimodetail, 
-    login, 
-    area, 
-    emprestimo,
-    ClienteViewSet, 
-    AreaViewSet, 
-    CobradorViewSet, 
-    EmprestimoViewSet, 
-    ParcelaViewSet,
-    ValeRuaViewSet
-)
-
 
 api_pathern = 'api/'
 
+urlpatterns = None
+app_name = 'app_uchoa'
 
-router = routers.DefaultRouter()
-router.register('clientes', ClienteViewSet)
-router.register('areas', AreaViewSet)
-router.register('cobradores', CobradorViewSet)
-router.register('emprestimos', EmprestimoViewSet)
-router.register('parcelas', ParcelaViewSet)
-router.register('vales', ValeRuaViewSet)
+try:
+    from django.contrib import admin
+    from django.urls import path, include
 
-urlpatterns = [
-    path('', home, name='home'),
-    path(api_pathern+'v1/', include(router.urls)),
-    path('cliente/<int:id>/', clientedetail, name='clientedetail'),
-    path('cliente/', cliente, name='cliente'),
-    path('emprestimo/', emprestimo, name='emprestimo'),
-    path('emprestimo/<int:pk>/', emprestimodetail, name='emprestimodetail'),
-    path('login/', login, name='login'),
-    path('area/<int:codigo>/', area, name='area'),
+    from rest_framework import routers
 
-]
+    from .views import (home,
+        cliente, 
+        clientedetail, 
+        emprestimodetail, 
+        login, 
+        area, 
+        emprestimo,
+        ClienteViewSet, 
+        AreaViewSet, 
+        CobradorViewSet, 
+        EmprestimoViewSet, 
+        ParcelaViewSet,
+        ValeRuaViewSet
+    )
+
+
+
+    router = routers.DefaultRouter()
+    router.register('clientes', ClienteViewSet)
+    router.register('areas', AreaViewSet)
+    router.register('cobradores', CobradorViewSet)
+    router.register('emprestimos', EmprestimoViewSet)
+    router.register('parcelas', ParcelaViewSet)
+    router.register('vales', ValeRuaViewSet)
+
+    urlpatterns = [
+        path('', home, name='home'),
+        path(api_pathern+'v1/', include(router.urls)),
+        path('cliente/<int:id>/', clientedetail, name='clientedetail'),
+        path('cliente/', cliente, name='cliente'),
+        path('emprestimo/', emprestimo, name='emprestimo'),
+        path('emprestimo/<int:pk>/', emprestimodetail, name='emprestimodetail'),
+        path('login/', login, name='login'),
+        path('area/<int:codigo>/', area, name='area'),
+    ]
+
+except Exception as e:
+    logger.error(app_name+" Error urls: "+str(e))
+    urlpatterns = []
